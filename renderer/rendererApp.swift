f79d7114d9361e91f2b57cee89df848490a88e83
zhvrnkov <v.zhvrnkv@gmail.com>
2023-11-24 20:26:45 +0600

    Initial Commit

---
+++ renderer/rendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  rendererApp.swift
+//  renderer
+//
+//  Created by Владислав Жаворонков on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct rendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
